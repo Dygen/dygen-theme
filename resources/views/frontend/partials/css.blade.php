41f6f0e05bb84bc811a93b6122673ae17b748544
Wes Hulette <jwhulette@gmail.com>
2017-01-19 12:37:39 -0500

    Css update

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,4 +1,4 @@
-<!-- Font Awesome -->
+{{-- <!-- Font Awesome -->
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 
 <!-- Prism JS -->
@@ -7,4 +7,11 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
 
 <!-- Application Styles -->
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}"> --}}
+
+<!-- Styles -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+<!-- Elixir styles -->
+<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+<link rel="stylesheet" href="{{ elixir('css/guest.css') }}">
--- resources/views/shared/meta-tags.blade.php
+++ resources/views/shared/meta-tags.blade.php
@@ -15,7 +15,7 @@
     <meta name="twitter:site" content="{{ $user->twitter or ''}}" />
     <meta name="twitter:title" content="{{ \Canvas\Models\Settings::blogTitle() }}" />
     <meta name="twitter:description" content="{{ \Canvas\Models\Settings::blogDescription() }}" />
-    <meta name="twitter:image" content="{{ url('vendor/canvas/assets/images/favicon.png') }}" />
+    <meta name="twitter:image" content="/favicon/favicon-96x96.png" />
 @endif
 
 <!-- SEO Tags -->
@@ -26,4 +26,18 @@
 <meta name="description" content="{{ \Canvas\Models\Settings::blogDescription() }}">
 
 <!-- Favicon -->
-<link rel="icon" type="image/png" href="{{ asset('vendor/canvas/assets/images/favicon.png') }}">
+{{-- <link rel="icon" type="image/png" href="{{ asset('vendor/canvas/assets/images/favicon.png') }}"> --}}
+<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+
+<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
