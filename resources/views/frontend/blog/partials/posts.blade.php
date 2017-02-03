292d4605116089aa4c779a6d0b9119170cc26aff
Wes Hulette <jwhulette@gmail.com>
2017-02-03 16:13:27 -0500

    Added image to listing page

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -1,19 +1,24 @@
 @foreach ($posts as $post)
-    <div class="post-preview">
-        <h2 class="post-title">
-            <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
-        </h2>
-        <p class="post-meta">
-            {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
-            <br>
-            @unless ($post->tags->isEmpty())
-                {!! implode(' ', $post->tagLinks()) !!}
-            @endunless
-        </p>
-        <p class="postSubtitle">
-            {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
-        </p>
-        <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+    <div class="col-xs-12 col-sm-4">
+        <img src="{{ asset($post->page_image) }}" class="post-hero img-responsive">
+    </div>
+    <div class="col-xs-12 col-sm-4">
+        <div class="post-preview">
+            <h2 class="post-title">
+                <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
+            </h2>
+            <p class="post-meta">
+                {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
+                <br>
+                @unless ($post->tags->isEmpty())
+                    {!! implode(' ', $post->tagLinks()) !!}
+                @endunless
+            </p>
+            <p class="postSubtitle">
+                {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
+            </p>
+            <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+        </div>
     </div>
     <hr>
 @endforeach
