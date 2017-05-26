529c065d79b76af466051870fb2801478f8c368f
Wes Hulette <jwhulette@gmail.com>
2017-05-26 10:09:53 -0400

    updated for new look

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -1,24 +1,43 @@
 @foreach ($posts as $post)
-    <div class="col-xs-12 col-sm-4">
-        <img src="{{ asset($post->page_image) }}" class="post-hero img-responsive">
-    </div>
-    <div class="col-xs-12 col-sm-4">
-        <div class="post-preview">
-            <h2 class="post-title">
-                <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
-            </h2>
-            <p class="post-meta">
-                {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
-                <br>
-                @unless ($post->tags->isEmpty())
-                    {!! implode(' ', $post->tagLinks()) !!}
-                @endunless
-            </p>
-            <p class="postSubtitle">
-                {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
-            </p>
-            <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+    <div class="row" style="display:table">
+        <div class="col-xs-12 col-sm-4 post_index_img">
+            <img src="{{ asset($post->page_image) }}" class="post-hero img-responsive">
+        </div>
+        <div class="col-xs-12 col-sm-12">
+            <div class="post-preview">
+                <h2 class="post-title">
+                    <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
+                </h2>
+                <p class="post-meta">
+                    {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
+                    <br>
+                    @unless ($post->tags->isEmpty())
+                        {!! implode(' ', $post->tagLinks()) !!}
+                    @endunless
+                </p>
+                <p class="postSubtitle">
+                    {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
+                </p>
+                <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+            </div>
         </div>
     </div>
-    <hr>
+
+    {{-- <div class="post-preview">
+        <h2 class="post-title">
+            <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
+        </h2>
+        <p class="post-meta">
+            {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
+            <br>
+            @unless ($post->tags->isEmpty())
+                {!! implode(' ', $post->tagLinks()) !!}
+            @endunless
+        </p>
+        <p class="postSubtitle">
+            {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
+        </p>
+        <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+    </div>
+    <hr> --}}
 @endforeach
--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,5 +1,27 @@
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+{{-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism-okaidia.min.css">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/prism.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}"> --}}
+<!-- Styles -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+<!-- Elixir styles -->
+<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+<link rel="stylesheet" href="{{ elixir('css/vendor.css') }}">
+<style media="screen">
+    #head-c {
+        padding-top: 60px;
+    }
+    .container {
+        background-color: #fff;
+    }
+    /* Small devices (tablets, 768px and up) */
+@media (min-width: 768px) {
+    .post_index_img {
+        display:table-cell;
+        vertical-align:middle;
+        float:none;
+    }
+}
+</style>
